f1388b8d28a062774ae713f81171abb16f0ac8f5
M1caela <micacalvoo@gmail.com>
2024-11-01 22:21:16 -0300

    La galeria ahora es responsive y moví la sección "fondos de pantalla" acá.(esto modifica el site map)

--- pag-multi/js/galeria-multi.js
+++ pag-multi/js/galeria-multi.js
@@ -1,10 +1,27 @@
+
 $(document).ready(function(){
     $('#fotos').slick({
-        slidesToShow: 3,
+        slidesToShow: 3,   // Muestra 3 imágenes en pantallas grandes
         slidesToScroll: 3,
         infinite: true,
         dots: true,
         arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024, // Pantallas hasta 1024px (ej. netbooks)
+                settings: {
+                    slidesToShow: 2,  // Muestra 2 imágenes en pantalla
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600, // Pantallas hasta 600px (ej. móviles)
+                settings: {
+                    slidesToShow: 1,  // Muestra 1 imagen en pantalla
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
 
@@ -15,6 +32,48 @@ $(document).ready(function(){
         infinite: true,
         dots: true,
         arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024, // Pantallas hasta 1024px (ej. netbooks)
+                settings: {
+                    slidesToShow: 2,  // Muestra 2 imágenes en pantalla
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600, // Pantallas hasta 600px (ej. móviles)
+                settings: {
+                    slidesToShow: 1,  // Muestra 1 imagen en pantalla
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function(){
+    $('#fondos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024, // Pantallas hasta 1024px (ej. netbooks)
+                settings: {
+                    slidesToShow: 2,  // Muestra 2 imágenes en pantalla
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600, // Pantallas hasta 600px (ej. móviles)
+                settings: {
+                    slidesToShow: 1,  // Muestra 1 imagen en pantalla
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
 

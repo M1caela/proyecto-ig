326a2889e94bb54ceff2f3e40479e3a5b789b8ba
M1caela <micacalvoo@gmail.com>
2024-11-01 21:29:21 -0300

    Agregué la galeria de fotos a multimedia, usando slick js

---
+++ pag-multi/js/galeria-multi.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('#fotos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        arrows: true,
+    });
+});
+
+$(document).ready(function(){
+    $('#ilust').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        arrows: true,
+    });
+});
+
